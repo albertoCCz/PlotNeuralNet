ff527cd1642b6a705de286d2b4865e3b0b835cf3
albertoCCz <albertocalzada95@gmail.com>
2022-07-19 11:08:38 +0200

    Finished my-net submodels

--- my-nets/liti_model.py
+++ my-nets/liti_model.py
@@ -15,33 +15,33 @@ arch = [
     to_begin(),
 
     # conv1
-    to_Conv("conv1d", caption="Conv stack 1", s_filter=5, n_filter=8, size="", to=nodes["a"], width=6, height=2, depth=32),
+    to_Conv("conv1d", caption="Conv stack 1", s_filter=5, n_filter=8, size="", to=nodes["a"], width=6, height=2, depth=24),
     *alt_colors_block(
         [
-            to_Dense(name="batch_norm", size="", to="(conv1d-east)",     depth=32, caption=""),
-            to_Dense(name="relu",       size="", to="(batch_norm-east)", depth=32, caption=""),
-            to_Dense(name="dropout",    size=32, to="(relu-east)",       depth=32, caption=""),
+            to_Dense(name="batch_norm", size="", to="(conv1d-east)",     depth=24, caption=""),
+            to_Dense(name="relu",       size="", to="(batch_norm-east)", depth=24, caption=""),
+            to_Dense(name="dropout",    size=24, to="(relu-east)",       depth=24, caption=""),
         ]
     ),
-    to_Pool(name="maxpool", s_pool=2, size=16, to="(dropout-east)", width=2, height=2, depth=16),
+    to_Pool(name="maxpool", s_pool=4, size=6, to="(dropout-east)", width=2, height=2, depth=6),
 
     to_connection_node("maxpool", nodes["b"]),
 
     # conv2
-    to_Conv("conv1d_1", caption="Conv stack 2", s_filter=10, n_filter=16, size="", to=nodes["b"], width=12, height=2, depth=16),
+    to_Conv("conv1d_1", caption="Conv stack 2", s_filter=10, n_filter=16, size="", to=nodes["b"], width=12, height=2, depth=6),
     *alt_colors_block(
         [
-            to_Dense(name="batch_norm_1", size="", to="(conv1d_1-east)",     depth=16, caption=""),
-            to_Dense(name="relu_1",       size="", to="(batch_norm_1-east)", depth=16, caption=""),
-            to_Dense(name="dropout_1",    size=16, to="(relu_1-east)",       depth=16, caption=""),
+            to_Dense(name="batch_norm_1", size="", to="(conv1d_1-east)",     depth=6, caption=""),
+            to_Dense(name="relu_1",       size=16, to="(batch_norm_1-east)", depth=6, caption=""),
+            to_Dense(name="dropout_1",    size="", to="(relu_1-east)",       depth=6, caption=""),
         ]
     ),
-    to_Pool(name="maxpool", s_pool=2, size=8, to="(dropout_1-east)", width=2, height=2, depth=8),
+    to_Pool(name="maxpool", s_pool=4, size=1, to="(dropout_1-east)", width=2, height=2, depth=2),
 
     to_connection_node("maxpool", nodes["c"]),
 
     # flatten
-    to_Dense("flatten", size=128, to=nodes["c"], depth=48, caption="Flatten"),
+    to_Dense("flatten", size=16, to=nodes["c"], depth=16, caption="Flatten"),
 
     to_connection_node("flatten", nodes["d"]),
 
--- my-nets/plus_model.py
+++ my-nets/plus_model.py
@@ -12,50 +12,46 @@ arch = [
     new_color("color2", "357ABB"),
     to_begin(),
 
+    to_Dense(name="dense", size="", to=nodes["a"],  depth=34, caption=""),
     *alt_colors_block(
         [
-            to_Dense(name="dense",      size=34, to=nodes["a"],          depth=34, caption=""),
-            to_Dense(name="batch_norm", size=34, to="(dense-east)",      depth=34, caption="Dense stack 1"),
-            to_Dense(name="relu",       size=34, to="(batch_norm-east)", depth=34, caption=""),
+            to_Dense(name="batch_norm", size="", to="(dense-east)",      depth=34, caption="Dense stack 1"),
+            to_Dense(name="relu",       size="", to="(batch_norm-east)", depth=34, caption=""),
             to_Dense(name="dropout",    size=34, to="(relu-east)",       depth=34, caption=""),
-        ],
-        colors=["color1", "color2"]
+        ]
     ),
 
     to_connection_node( "dropout", nodes["b"]),
 
+    to_Dense(name="dense_1", size="", to=nodes["b"], depth=38, caption=""),
     *alt_colors_block(
         [
-            to_Dense(name="dense_1",      size=38, to=nodes["b"],            depth=38, caption=""),
-            to_Dense(name="batch_norm_1", size=38, to="(dense_1-east)",      depth=38, caption="Dense stack 2"),
-            to_Dense(name="relu_1",       size=38, to="(batch_norm_1-east)", depth=38, caption=""),
+            to_Dense(name="batch_norm_1", size="", to="(dense_1-east)",      depth=38, caption="Dense stack 2"),
+            to_Dense(name="relu_1",       size="", to="(batch_norm_1-east)", depth=38, caption=""),
             to_Dense(name="dropout_1",    size=38, to="(relu_1-east)",       depth=38, caption=""),
-        ],
-        colors=["color1", "color2"]
+        ]
     ),
 
     to_connection_node( "dropout_1", nodes["c"]),
 
+    to_Dense(name="dense_2", size="", to=nodes["c"], depth=38, caption=""),
     *alt_colors_block(
         [
-            to_Dense(name="dense_2",      size=38, to=nodes["c"],            depth=38, caption=""),
-            to_Dense(name="batch_norm_2", size=38, to="(dense_2-east)",      depth=38, caption="Dense stack 3"),
-            to_Dense(name="relu_2",       size=38, to="(batch_norm_2-east)", depth=38, caption=""),
+            to_Dense(name="batch_norm_2", size="", to="(dense_2-east)",      depth=38, caption="Dense stack 3"),
+            to_Dense(name="relu_2",       size="", to="(batch_norm_2-east)", depth=38, caption=""),
             to_Dense(name="dropout_2",    size=38, to="(relu_2-east)",       depth=38, caption=""),
-        ],
-        colors=["color1", "color2"]
+        ]
     ),
 
     to_connection_node( "dropout_2", nodes["d"]),
 
+    to_Dense(name="dense_3", size="", to=nodes["d"], depth=8, caption=""),
     *alt_colors_block(
         [
-            to_Dense(name="dense_3",      size=8, to=nodes["d"],            depth=8, caption=""),
-            to_Dense(name="batch_norm_3", size=8, to="(dense_3-east)",      depth=8, caption="Dense stack 4"),
-            to_Dense(name="relu_3",       size=8, to="(batch_norm_3-east)", depth=8, caption=""),
-            to_Dense(name="dropout_3",    size=8, to="(relu_3-east)",       depth=8, caption=""),
-        ],
-        colors=["color1", "color2"]
+            to_Dense(name="batch_norm_3", size="", to="(dense_3-east)",      depth=8, caption="Dense stack 4"),
+            to_Dense(name="relu_3",       size="", to="(batch_norm_3-east)", depth=8, caption=""),
+            to_Dense(name="dropout_3",    size=8, to="(relu_3-east)",        depth=8, caption=""),
+        ]
     ),
 
     to_end(),
