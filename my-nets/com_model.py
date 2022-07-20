a9c2af6df8f8155509d99e1c96f8f8db8a6974f7
albertoCCz <albertocalzada95@gmail.com>
2022-07-20 15:19:01 +0200

    Add input images and improve boxes coloring

--- my-nets/com_model.py
+++ my-nets/com_model.py
@@ -30,10 +30,20 @@ nodes_mixg = apply_offset(float(list(nodes_spec.values())[-1].replace('+\skipshi
 # print(f"liti: {nodes_liti}")
 # print(f"mixg: {nodes_mixg}")
 
+# define the input images
+input_offset = -2.5
+width  = 7
+height = 3.5
+inputs = [
+    insert_canvas_image("images/lifetime_mirror.jpg",              list(apply_offset(input_offset, 'x', get_node(nodes_liti, 0)).values())[0], width=width, height=height),
+    insert_canvas_image("images/fluorescence_spectrum_mirror.jpg", list(apply_offset(input_offset, 'x', get_node(nodes_spec, 0)).values())[0], width=width, height=height),
+    insert_canvas_image("images/plus_features_mirror.jpg",         list(apply_offset(input_offset, 'x', get_node(nodes_plus, 0)).values())[0], width=width, height=height),
+]
+
 # define each submodel architecture
 liti = [
     # conv1
-    to_Conv("conv1d_10", caption="Conv stack 1", s_filter=5, n_filter=8, size="", to=nodes_liti["a"], width=6, height=2, depth=24),
+    to_Conv("conv1d_10", caption="Conv stack 1", s_filter=5, n_filter=8, size="", to=nodes_liti["a"], width=6, height=2, depth=24, color="conv_color"),
     *alt_colors_block(
         [
             to_Dense(name="batch_norm_10", size="", to="(conv1d_10-east)",     depth=24, caption=""),
@@ -46,7 +56,7 @@ liti = [
     to_connection_node("maxpool_10", nodes_liti["b"]),
 
     # conv2
-    to_Conv("conv1d_11", caption="Conv stack 2", s_filter=10, n_filter=16, size="", to=nodes_liti["b"], width=12, height=2, depth=6),
+    to_Conv("conv1d_11", caption="Conv stack 2", s_filter=10, n_filter=16, size="", to=nodes_liti["b"], width=12, height=2, depth=6, color="conv_color"),
     *alt_colors_block(
         [
             to_Dense(name="batch_norm_11", size="", to="(conv1d_11-east)",     depth=6, caption=""),
@@ -59,12 +69,12 @@ liti = [
     to_connection_node("maxpool_11", nodes_liti["c"]),
 
     # flatten
-    to_Dense("flatten_12", size=16, to=nodes_liti["c"], depth=16, caption="Flatten 1"),
+    to_Dense("flatten_12", size=16, to=nodes_liti["c"], depth=16, color="flatten", caption="Flatten 1"),
 
     to_connection_node("flatten_12", nodes_liti["d"]),
 
     # dense1
-    to_Dense(name="dense_13", size="", to=nodes_liti["d"], depth=32, caption=""),
+    to_Dense(name="dense_13", size="", to=nodes_liti["d"], depth=32, caption="", color="superdarkgreen"),
     *alt_colors_block(
         [
             to_Dense(name="batch_norm_13", size="", to="(dense_13-east)",        depth=32, caption="Dense stack 1"),
@@ -76,7 +86,7 @@ liti = [
     to_connection_node("dropout_13", nodes_liti["e"]),
 
     # dense2
-    to_Dense(name="dense_14", size="", to=nodes_liti["e"], depth=8, caption="Dense stack 2"),
+    to_Dense(name="dense_14", size="", to=nodes_liti["e"], depth=8, caption="Dense stack 2", color="superdarkgreen"),
     *alt_colors_block(
         [
             to_Dense(name="batch_norm_14", size="", to="(dense_14-east)",      depth=8, caption=""),
@@ -88,7 +98,7 @@ liti = [
 
 spec = [
     # conv1
-    to_Conv("conv1d_5", caption="Conv stack 3", s_filter=5, n_filter=8, size="", to=nodes_spec["a"], width=6, height=2, depth=32),
+    to_Conv("conv1d_5", caption="Conv stack 3", s_filter=5, n_filter=8, size="", to=nodes_spec["a"], width=6, height=2, depth=32, color="conv_color"),
     *alt_colors_block(
         [
             to_Dense(name="batch_norm_5", size="", to="(conv1d_5-east)",     depth=32, caption=""),
@@ -101,7 +111,7 @@ spec = [
     to_connection_node("maxpool_5", nodes_spec["b"]),
 
     # conv2
-    to_Conv("conv1d_6", caption="Conv stack 4", s_filter=10, n_filter=16, size="", to=nodes_spec["b"], width=12, height=2, depth=16),
+    to_Conv("conv1d_6", caption="Conv stack 4", s_filter=10, n_filter=16, size="", to=nodes_spec["b"], width=12, height=2, depth=16, color="conv_color"),
     *alt_colors_block(
         [
             to_Dense(name="batch_norm_6", size="", to="(conv1d_6-east)",     depth=16, caption=""),
@@ -114,12 +124,12 @@ spec = [
     to_connection_node("maxpool_6", nodes_spec["c"]),
 
     # flatten
-    to_Dense("flatten_7", size=128, to=nodes_spec["c"], depth=48, caption="Flatten 2"),
+    to_Dense("flatten_7", size=128, to=nodes_spec["c"], depth=48, color="flatten", caption="Flatten 2"),
 
     to_connection_node("flatten_7", nodes_spec["d"]),
 
     # dense1
-    to_Dense(name="dense_8", size="", to=nodes_spec["d"], depth=32, caption=""),
+    to_Dense(name="dense_8", size="", to=nodes_spec["d"], depth=32, caption="", color="superdarkgreen"),
     *alt_colors_block(
         [
             to_Dense(name="batch_norm_8", size="", to="(dense_8-east)",        depth=32, caption="Dense stack 3"),
@@ -131,7 +141,7 @@ spec = [
     to_connection_node("dropout_8", nodes_spec["e"]),
 
     # dense2
-    to_Dense(name="dense_9", size="", to=nodes_spec["e"], depth=8, caption="Dense stack 4"),
+    to_Dense(name="dense_9", size="", to=nodes_spec["e"], depth=8, caption="Dense stack 4", color="superdarkgreen"),
     *alt_colors_block(
         [
             to_Dense(name="batch_norm_9", size="", to="(dense_9-east)",      depth=8, caption=""),
@@ -142,7 +152,7 @@ spec = [
 ]
 
 plus = [
-    to_Dense(name="dense_1", size="", to=nodes_plus["a"],  depth=34, caption=""),
+    to_Dense(name="dense_1", size="", to=nodes_plus["a"],  depth=34, caption="", color="superdarkgreen"),
     *alt_colors_block(
         [
             to_Dense(name="batch_norm_1", size="", to="(dense_1-east)",      depth=34, caption="Dense stack 5"),
@@ -153,7 +163,7 @@ plus = [
 
     to_connection_node("dropout_1", nodes_plus["b"]),
 
-    to_Dense(name="dense_2", size="", to=nodes_plus["b"], depth=38, caption=""),
+    to_Dense(name="dense_2", size="", to=nodes_plus["b"], depth=38, caption="", color="superdarkgreen"),
     *alt_colors_block(
         [
             to_Dense(name="batch_norm_2", size="", to="(dense_2-east)",      depth=38, caption="Dense stack 6"),
@@ -164,7 +174,7 @@ plus = [
 
     to_connection_node( "dropout_2", nodes_plus["c"]),
 
-    to_Dense(name="dense_3", size="", to=nodes_plus["c"], depth=38, caption=""),
+    to_Dense(name="dense_3", size="", to=nodes_plus["c"], depth=38, caption="", color="superdarkgreen"),
     *alt_colors_block(
         [
             to_Dense(name="batch_norm_3", size="", to="(dense_3-east)",      depth=38, caption="Dense stack 7"),
@@ -175,7 +185,7 @@ plus = [
 
     to_connection_node( "dropout_3", nodes_plus["d"]),
 
-    to_Dense(name="dense_4", size="", to=nodes_plus["d"], depth=8, caption=""),
+    to_Dense(name="dense_4", size="", to=nodes_plus["d"], depth=8, caption="", color="superdarkgreen"),
     *alt_colors_block(
         [
             to_Dense(name="batch_norm_4", size="", to="(dense_4-east)",      depth=8, caption="Dense stack 8"),
@@ -198,10 +208,10 @@ lines = [
 ]
 
 mixg = [
-    to_Dense(name="dense_9",      size="",  to=nodes_mixg["a"],       depth=8),
-    to_Dense(name="dense_10",     size="",  to="(dense_9-east)",      depth=8, caption="Mixing stack"),
+    to_Dense(name="dense_9",      size="",  to=nodes_mixg["a"],       depth=8, color="superdarkgreen"),
+    to_Dense(name="dense_10",     size="",  to="(dense_9-east)",      depth=8, caption="Mixing stack", color="superdarkgreen"),
     to_Dense(name="batch_norm_9", size="",  to="(dense_10-east)",     depth=8, color="color1"),
-    to_Dense(name="softmax_1",    size="8", to="(batch_norm_9-east)", depth=8, color="forestgreen"),
+    to_Dense(name="softmax_1",    size="8", to="(batch_norm_9-east)", depth=8),
 ]
 
 # define the global architecture
@@ -211,9 +221,14 @@ arch = [
     new_color("color1", "A2C7EA"),
     new_color("color2", "357ABB"),
     new_color("color_pool", "F44724"),
+    new_color("flatten", "D079F9"),
+    new_color("conv_color", "F1941D"),
     new_color("dark_yellow", "F9D046"),
     new_color("forestgreen", "24BB09"),
+    new_color("superdarkgreen", "14A616"),
     to_begin(),
+
+    *inputs,
 
     *liti,
     *spec,
